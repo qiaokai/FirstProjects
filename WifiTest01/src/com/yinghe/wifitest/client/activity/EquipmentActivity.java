d9726164a8fb483e54adbb9bae8d36f21c6efa31
qiaokai <13418790126@163.com>
2016-07-25 18:38:55 +0800

    增加设备信息页面布局

--- WifiTest01/src/com/yinghe/wifitest/client/activity/EquipmentActivity.java
+++ WifiTest01/src/com/yinghe/wifitest/client/activity/EquipmentActivity.java
@@ -4,6 +4,8 @@ import com.example.wifitest01.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EquipmentActivity extends Activity {
 	@Override
@@ -11,5 +13,14 @@ public class EquipmentActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_equipment);
 
+		TextView a = (TextView) findViewById(R.id.text_timer_setting);
+		a.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 }
--- WifiTest01/src/com/yinghe/wifitest/client/entity/EquipmentInfo.java
+++ WifiTest01/src/com/yinghe/wifitest/client/entity/EquipmentInfo.java
@@ -2,14 +2,14 @@ package com.yinghe.wifitest.client.entity;
 
 public class EquipmentInfo {
 	private String uid;// 账号ID
-	private String equipmentId;// 设备ID
+	private String equipmentId;// 设备编号
 	private String equipmentName;// 设备名称
 	private String equipmentState;// 设备状态
 	private String currentVoltage;// 当前设备电压
 	private String currentElectric;// 当前设备电流
 	private int lastElectricQuantity;// 上次电量
-	private int currentElectricQuantity;// 当前电量
+	private int currentQuantity;// 当前电量
 	private int maxElectricQuantity;// 最大电量
-	private int availableElectricQuantity;// 当前可用电量
+	private int availableQuantity;// 可用电量
 
 }
